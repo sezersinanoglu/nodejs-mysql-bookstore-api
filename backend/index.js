495996e63f7406ec8bed9f18d9229037c8294cac
Sezer SINANOGLU <sezersinanoglu@gmail.com>
2022-09-22 17:22:56 +0300

    node js with mysql basic project

---
+++ backend/index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "!Genmot1987",
+  database: "testdb",
+});
+
+app.get("/books", (req, res) => {
+  const q = "select * from books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json("Book has been created succesfully!");
+  });
+});
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM books where book_id= ?";
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json("Book has been deleted succesfully!");
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE book_id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values,bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.json("hello this is the backend");
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend");
+});
